63781d3ea440b93e4720e595ab4ab4c35159380b
renan jegouzo <renan@aestesis.org>
2024-11-26 01:15:42 +0100

    meta ok

--- request.mjs
+++ request.mjs
@@ -2,30 +2,35 @@ import net from 'node:net';
 import { EventEmitter } from 'node:events';
 import tls from 'node:tls';
 
-export class HttpRequest extends EventEmitter {
+export class IcyRequest extends EventEmitter {
     constructor(url) {
         super();
         this.url = new URL(url);
     }
     async send(options = {}) {
-        options = { ...{ method: 'GET' }, ...options };
+        options = { ...{ method: 'GET', timeout: 2000 }, ...options };
         const url = this.url;
         let socket;
         if (url.protocol == 'https:') {
             const port = parseInt(url.port) || 443;
             socket = this.socket = tls.connect(port, url.hostname);
         } else {
-            this.socket = new net.Socket();
             const port = parseInt(url.port) || 80;
+            this.socket = new net.Socket();
             socket = this.socket.connect(port, url.hostname);
         }
+        const timeout = setTimeout(() => {
+            this.emit('error', 'timeout');
+            socket.destroy();
+        }, options.timeout);
         socket.on('connect', () => {
+            clearTimeout(timeout);
             socket.write(`${options.method} ${url.pathname}${url.search} HTTP/1.0\r\nHost: ${url.hostname}\r\nUser-Agent: Casty\r\nIcy-MetaData: 1\r\n\r\n`);
         });
         let data = new Buffer.alloc(0);
         socket.on('data', (d) => {
+            data = Buffer.concat([data, d]);
             if (!this.response) {
-                data += d;
                 const index = data.indexOf('\r\n\r\n');
                 if (index) {
                     const h = data.slice(0, index + 4);
@@ -33,21 +38,62 @@ export class HttpRequest extends EventEmitter {
                     const raw = h.toString('utf8');
                     this.response = this.parseResponse(raw);
                     this.emit('response', this.response);
-                    if (data.length) {
+                    const headers = this.response.headers;
+                    this.metaInt = parseInt(headers['icy-metaint']) || 0;
+                    if (!this.metaInt && data.length) {
                         this.emit('data', data);
                         data = undefined;
                     }
                 }
             } else {
-                this.emit('data', d);
+                if (this.metaInt) {
+                    const metaInt = this.metaInt;
+                    while (data.length > metaInt) {
+                        //const byte = Uint8Array.from(data.slice(metaInt, metaInt + 1))[0]
+                        const byte = data.readUInt8(metaInt)
+                        const metaSize = byte * 16 + 1;
+                        if (data.length >= metaInt + metaSize) {
+                            if (metaSize > 1) {
+                                const buffer = data.slice(metaInt + 1, metaInt + metaSize);
+                                const meta = this.parseMeta(buffer);
+                                if (meta) {
+                                    this.emit('meta', meta);
+                                }
+                            }
+                            this.emit('data', data.slice(0, metaInt));
+                            data = data.slice(metaInt + metaSize);
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    this.emit('data', d);
+                }
             }
         });
-        socket.on('end', (e) => {
-            this.emit('end', e);
+        socket.on('end', () => {
+            this.emit('end');
         });
-        socket.on('error', (e) => {
-            this.emit('error', e);
+        socket.on('error', (error) => {
+            console.log('error', error);
+            this.emit('error', error);
         });
+    }
+    parseMeta(buffer) {
+        const text = buffer.toString('ascii').split(';');
+        const meta = {};
+        for (const t of text) {
+            const p = t.split('=');
+            if (p.length == 2) {
+                const k = p[0].trim().toLowerCase();
+                let v = p[1].trim();
+                if (v.length && v[0] == "'" && v[v.length - 1] == "'") {
+                    v = v.substring(1, v.length - 1);
+                }
+                meta[k] = v;
+            }
+        }
+        return meta;
     }
     parseResponse(text) {
         const response = { headers: {} };
@@ -58,15 +104,11 @@ export class HttpRequest extends EventEmitter {
         for (const h of hh.slice(1)) {
             if (h.length) {
                 const p = h.split(':');
-                response.headers[p[0].trim()] = p.slice(1).join(':').trim();
+                response.headers[p[0].trim().toLowerCase()] = p.slice(1).join(':').trim();
             }
         }
         return response;
     }
-    request(url, options) {
-        return;
-    }
-
     destroy() {
         this.socket.destroy();
     }
--- test.mjs
+++ test.mjs
@@ -1,14 +1,20 @@
-import { HttpRequest } from './request.mjs';
+import { IcyRequest } from './request.mjs';
 
-//const request = new HttpRequest('http://jenx.globalconnection.world:8000');
-const request = new HttpRequest('https://io.waves.pw/radio.ogg');
+//const request = new IcyRequest('http://radio.openstageis.land');
+//const request = new IcyRequest('https://io.waves.pw/radio.ogg');
+//const request = new IcyRequest('http://jenx.globalconnection.world:8000');
+const request = new IcyRequest('http://djthirteen.lightmanstreams.com:8460');
 
 
 request.on('response', (r) => {
     console.log('response', JSON.stringify(r));
 });
-request.on('data', (d) => {
-    request.close();
+request.on('meta', (d) => {
+    console.log('meta', d);
+    //request.close();
 });
+request.on('error', (error) => {
+    console.log(error);
+})
+request.send({ method: 'GET' });
 
-request.send({method:'GET'});
